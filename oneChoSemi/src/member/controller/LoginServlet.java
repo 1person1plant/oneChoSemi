7980eb485a052fdb508fa67efa0534cc0c7c1ae6
Helkanes <kangkwangsan@gmail.com>
2020-06-03 10:11:14 +0900

    200603 시작 전 커밋

--- oneChoSemi/src/member/controller/LoginServlet.java
+++ oneChoSemi/src/member/controller/LoginServlet.java
@@ -25,7 +25,6 @@ public class LoginServlet extends HttpServlet {
      */
     public LoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -63,7 +62,6 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
