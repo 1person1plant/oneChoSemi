3a692a93a502aec53401b30a7055b57492788672
Helkanes <kangkwangsan@gmail.com>
2020-06-07 13:00:19 +0900

    DEV_KKS에서 합침

--- oneChoSemi/src/cartList/model/dao/CartDao.java
+++ oneChoSemi/src/cartList/model/dao/CartDao.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import cartList.model.vo.Cart;
+import order.model.vo.Order;
 
 import static common.JDBCTemplate.close;
 
@@ -18,7 +19,7 @@ public class CartDao {
 		
 		ArrayList<Cart> cartList = new ArrayList<>();
 		
-		String query = "SELECT MEMBER_NO , ITEM_NO , ITEM_NAME , ITEM_PRICE , ITEM_MAX , CARTLIST_COUNT , IMAGE_NAME FROM MEMBER_CARTLIST WHERE MEMBER_NO =?";
+		String query = "SELECT MEMBER_NO, CARTLIST_NO , ITEM_NO , ITEM_NAME , ITEM_PRICE , ITEM_MAX , CARTLIST_COUNT , IMAGE_NAME FROM MEMBER_CARTLIST WHERE MEMBER_NO =?";
 		
 		try {
 			pstmt = conn.prepareStatement(query);
@@ -28,6 +29,7 @@ public class CartDao {
 			
 			while(rSet.next()) {
 				Cart c = new Cart(rSet.getString("MEMBER_NO")
+								, rSet.getString("CARTLIST_NO")
 								, rSet.getString("ITEM_NO")
 								, rSet.getString("ITEM_NAME")
 								, rSet.getInt("ITEM_PRICE")
@@ -47,6 +49,32 @@ public class CartDao {
 		}
 
 		return cartList;
+	}
+
+	public int deleteCart(Connection conn, ArrayList<Cart> deleteCart) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = "";
+		// TODO 미완성
+		
+		try {
+			for(int i = 0 ; i < deleteCart.size() ; i++) {
+				Cart ca = deleteCart.get(i);
+
+				pstmt = conn.prepareStatement(query);
+				pstmt.setString(1, ca.getCartListNo());
+				
+				result += pstmt.executeUpdate();
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+
+		return result;
 	}
 
 }
